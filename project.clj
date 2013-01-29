0b57578365abc7523973c0a0b34ded4638363bbe
Ravi Kotecha <kotecha.ravi@gmail.com>
2013-01-29 16:34:26 +0000

    can get droplets

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject digitalocean-api "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "digital ocean cloud service API wrapper"
+  :url "https://github.com/r4vi/digitalocean-api"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/data.json "0.2.1"]
+                 [clj-http "0.6.3"]])
--- src/digitalocean_api/core.clj
+++ src/digitalocean_api/core.clj
@@ -1,6 +1,24 @@
-(ns digitalocean-api.core)
+(ns digitalocean-api.core
+  (:require [clj-http.client :as client]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def ^:dynamic *base-url* "https://api.digitalocean.com")
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn- fix-keyword
+  "Takes a parameter name and replaces the - with a _"
+  [param-name]
+  (keyword (.replace (name param-name) \- \_)))
+
+(defn- transform-map 
+  "transforms a clojure map into something that can be sent to a
+  web server as query-params"
+  [params]
+  (into {} (for [[k v] params] [(fix-keyword (name k)) v])))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn show-droplets [creds & droplet-id]
+  (let [droplet-id (or (first droplet-id) nil)]
+  (client/get (apply str (interpose "/" [*base-url* "droplets" droplet-id])) 
+              {:as :auto
+               :query-params (transform-map creds)})))
