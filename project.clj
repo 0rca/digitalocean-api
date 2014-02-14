5e5fb81c29cf7cd37a1359a7a25d8f704d2dcf69
Alex Vzorov <alex.vzorov@gmail.com>
2014-02-14 20:00:32 +0700

    Drop data.json dependency
    and bump version

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject digitalocean-api "0.1.0-SNAPSHOT"
+(defproject digitalocean-api "0.1.1-SNAPSHOT"
   :description "digital ocean cloud service API wrapper"
   :url "https://github.com/r4vi/digitalocean-api"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/data.json "0.2.1"]
-                 [clj-http "0.6.3"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clj-http "0.7.9"]])
